a369edfaf376c4f2dc18979d45e838389f2e07c6
evie tan <evie@bedshape.xyz>
2025-06-14 21:18:06 +0800

    added deployment script, updated README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# evie's dotfiles
+
+## usage
+1. pull the repo to $HOME
+2. chmod +x $HOME/dots/deploy.sh
+3. $HOME/dots/deploy.sh
+
+## mapping - pull repo and map accordingly with symlinks
+* $HOME/dots/config/* -> $HOME/.config/
+* $HOME/dots/zshrc -> $HOME/.zshrc
+* $HOME/dots/gitconfig -> $HOME/.gitconfig
+
+## todo
+* [x] deployment script
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+DOTSHOME="$HOME/dots"
+DOTSCONFIG="$HOME/dots/config"
+
+echo "deploying dotfiles"
+
+ln -sf "$DOTSHOME/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTSHOME/gitconfig" "$HOME/.gitconfig"
+
+mkdir -p "$HOME/.config"
+for item in "$DOTSCONFIG"/*; do
+    name=$(basename "$item")
+    ln -sf "$item" "$HOME/.config/$name"
+done
+
+echo "dotfiles deployed"
